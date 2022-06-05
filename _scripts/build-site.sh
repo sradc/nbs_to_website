f9e8e5a0267c4e7f212481e2de93648c63b50d81
sradc <sidneyradcliffe@sky.com>
2022-06-05 17:01:49 +0100

    refactor: moved build-site into standalone script from hook

---
+++ _scripts/build-site.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Remove the old website files
+rm index.html script.js stylesheet.css
+rm -rf posts
+# Convert the nb to a script
+jupyter nbconvert --to script _notebooks/nbs-to-website.ipynb
+[ $? -ne 0 ] && echo "Failed to convert notebook to script. Aborting post-commit hook." && exit 1
+# Use the script to build the website
+python _notebooks/nbs-to-website.py
+[ $? -ne 0 ] && echo "Failed to build site. Aborting post-commit hook." && exit 1
+# Delete the script
+rm _notebooks/nbs-to-website.py
